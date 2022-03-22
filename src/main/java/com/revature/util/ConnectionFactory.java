b0186940560daf3be92d87a718ae364f9250449a
Justin <steelindian66@gmail.com>
2022-03-22 16:25:17 -0500

    Added database connection

--- src/main/java/com/revature/util/ConnectionFactory.java
+++ src/main/java/com/revature/util/ConnectionFactory.java
@@ -9,7 +9,7 @@ public class ConnectionFactory {
     private static Connection instance;
 
     private static final String url =
-            "jdbc:postgresql://java-react.cofxvzttfcqt.us-east-1.rds.amazonaws.com:5432/TBD";
+            "jdbc:postgresql://java-react.cofxvzttfcqt.us-east-1.rds.amazonaws.com:5432/postgres?currentSchema=runtime_terrors_p1";
     private static final String username = "postgres";
     private static final String password = "JS!6mm2194n02";
 
