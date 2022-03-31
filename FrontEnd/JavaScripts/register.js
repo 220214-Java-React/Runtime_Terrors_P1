6bad00ce6246db5eeed391a6e9efdcc53f0e1a99
Justin <steelindian66@gmail.com>
2022-03-31 09:11:27 -0500

    Added Javascript to post to the servlet

--- FrontEnd/JavaScripts/register.js
+++ FrontEnd/JavaScripts/register.js
@@ -8,3 +8,36 @@ function radio(){
         document.getElementById("FinanceManager").value =1;
     }
 }
+
+const BASE_API_URL = "http://localhost:8080/projectone.main";
+const Recourse_URL = `${BASE_API_URL}/users`;
+
+async function createUser(){
+    const username = document.getElementById('Username').value;
+    const email = document.getElementById('Email').value;
+    const password = document.getElementById('Password').value;
+    const fName = document.getElementById('fName').value;
+    const lName = document.getElementById('lName').value;
+    const userId = document.getElementById('role_id').value;
+    
+}
+
+const user ={
+    username: username,
+    password: password,
+    first_name: fName,
+    last_name: lName,
+    role_id: role_id,
+};
+
+const userJSON = JSON.stringify(user);
+
+//posting information to servlet
+
+const response = await fetch(RESOURCE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: userJSON,
+  });
