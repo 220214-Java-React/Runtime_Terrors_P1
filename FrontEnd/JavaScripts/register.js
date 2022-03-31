bd84900dfaca6a6f6fa55d124aa3888e4abf70e9
Justin <steelindian66@gmail.com>
2022-03-31 17:02:48 -0500

    Currently attempting to fix javascript to push information to the database currently error is due to not linking to local host properly

--- FrontEnd/JavaScripts/register.js
+++ FrontEnd/JavaScripts/register.js
@@ -1,35 +1,40 @@
 
-const BASE_API_URL = "http://localhost:8080/api";
-const Recourse_URL = `${BASE_API_URL}/users`;
+const BASE_API_URL = "http://localhost:8080/api/users";
+const RESOURCE_URL = `${BASE_API_URL}/users`;
 
 async function createUser(){
-    const username = document.getElementById('Username').value;
-    const email = document.getElementById('Email').value;
-    const password = document.getElementById('Password').value;
-    const fName = document.getElementById('fName').value;
-    const lName = document.getElementById('lName').value;
-    const userId = document.getElementById('role_id').value;
+    const username = document.getElementById('username').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pwd').value;
+    const firstName = document.getElementById('fName').value;
+    const lastName = document.getElementById('lName').value;
+    const roleID = document.getElementById('role_id').value;
     
 
 
     const user ={
         username: username,
+        email: email,
         password: password,
-        first_name: fName,
-        last_name: lName,
-        role_id: role_id,
+        first_name: firstName,
+        last_name: lastName,
+        role_id: roleID,
     };
 
     const userJSON = JSON.stringify(user);
 
     //posting information to servlet
 
-    const response = await fetch(RESOURCE_URL, {
+    const response = await fetch(BASE_API_URL, {
         method: "POST",
         headers: {
         "Content-Type": "application/json",
         },
         body: userJSON,
     });
+    if (response.status == 200) {
 
+      } else {
+
+      }
 }
